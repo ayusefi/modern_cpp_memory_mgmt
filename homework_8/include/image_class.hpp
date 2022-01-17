8e8fed28f5e2a468ebbc6bdb7202813d333bcf0a
Abdullah Yusefi <a.yusefi1991@gmail.com>
2022-01-17 07:47:16 +0300

    NOT Completed!

--- homework_8/include/image_class.hpp
+++ homework_8/include/image_class.hpp
@@ -5,19 +5,31 @@ public:
   Image();
   Image(const std::string& filename);
 
+  // bool FillFromPgm(const std::string& file_name);
+  // void WriteToPgm(const std::string& file_name);
+  // std::vector<float> ComputeHistogram(int bins) const;
+  // void DownScale(int scale);
+  // void UpScale(int scale);
+
+public:
+  struct Pixel
+  {
+    int red;
+    int green;
+    int blue;
+  };
+
   bool empty();
   const int rows();
   const int cols();
-  uint8_t at(int row, int col) const;
-  uint8_t& at(int row, int col);
-  bool FillFromPgm(const std::string& file_name);
-  void WriteToPgm(const std::string& file_name);
-  std::vector<float> ComputeHistogram(int bins) const;
-  void DownScale(int scale);
-  void UpScale(int scale);
 
-public:
-  std::vector<uint8_t> data_;
+  Pixel at(int row, int col) const;
+  Pixel& at(int row, int col);
+
+private:
+  // std::vector<uint8_t> data_;
+  std::vector<Pixel> data_;
+
   int rows_;
   int cols_;
 };
--- homework_8/task_2/src/image_class.cpp
+++ homework_8/task_2/src/image_class.cpp
@@ -9,11 +9,89 @@ Image::Image()
 }
 Image::Image(const std::string& filename)
 {
-  igg::io_tools::ImageData image = igg::io_tools::ReadFromPgm(filename);
-  data_ = image.data;
-  rows_ = image.rows;
-  cols_ = image.cols;
+  // igg::io_tools::ImageData image = igg::io_tools::ReadFromPgm(filename);
+  // data_ = image.data;
+  // rows_ = image.rows;
+  // cols_ = image.cols;
 }
+
+// bool Image::FillFromPgm(const std::string& file_name)
+// {
+//   igg::io_tools::ImageData image = igg::io_tools::ReadFromPgm(file_name);
+//   data_ = image.data;
+//   rows_ = image.rows;
+//   cols_ = image.cols;
+//   if (!data_.empty())
+//   {
+//     return true;
+//   }
+//   else
+//   {
+//     std::cerr << "[ERROR] Could not load data!\n";
+//     return false;
+//   }
+// }
+
+// void Image::WriteToPgm(const std::string& file_name)
+// {
+//   igg::io_tools::ImageData image;
+//   image.data = data_;
+//   image.rows = rows_;
+//   image.cols = cols_;
+//   bool written = igg::io_tools::WriteToPgm(image, file_name);
+// }
+
+// std::vector<float> Image::ComputeHistogram(int bins) const
+// {
+//   int binSize = 255 / bins;
+//   std::vector<float> histogram(bins, 0);
+
+//   // for each element of data_
+//   for (int i = 0; i < data_.size(); i++)
+//   {
+//     // for each bin
+//     for (int z = 0; z < bins; z++)
+//     {
+//       // check both upper and lower limits
+//       if (data_[i] >= z * binSize && data_[i] < (z + 1) * binSize)
+//       {
+//         // increment the index that contains the point
+//         histogram[z]++;
+//       }
+//     }
+//   }
+//   return histogram;
+// }
+
+// void Image::DownScale(int scale)
+// {
+//   std::vector<uint8_t> scaled_data(((rows_ + 1) / scale) * ((cols_ + 1) / scale), 0);
+//   for (int r = 0; r < (rows_ / scale); r++)
+//   {
+//     for (int c = 0; c < (cols_ / scale); c++)
+//     {
+//       scaled_data[r * (cols_ / scale) + c] = at(r * scale, c * scale);
+//     }
+//   }
+//   data_ = scaled_data;
+//   rows_ = rows_ / scale;
+//   cols_ = cols_ / scale;
+// }
+
+// void Image::UpScale(int scale)
+// {
+//   std::vector<uint8_t> scaled_data(((rows_ + 1) * scale) * ((cols_ + 1) * scale), 0);
+//   for (int r = 0; r < (rows_ * scale); r++)
+//   {
+//     for (int c = 0; c < (cols_ * scale); c++)
+//     {
+//       scaled_data[r * (cols_ * scale) + c] = at(std::floor(r / scale), std::floor(c / scale));
+//     }
+//   }
+//   data_ = scaled_data;
+//   rows_ = rows_ * scale;
+//   cols_ = cols_ * scale;
+// }
 
 bool Image::empty()
 {
@@ -28,92 +106,4 @@ const int Image::rows()
 const int Image::cols()
 {
   return cols_;
-}
-
-uint8_t Image::at(int row, int col) const
-{
-  return data_[row * cols_ + col];
-}
-
-uint8_t& Image::at(int row, int col)
-{
-  return data_[row * cols_ + col];
-}
-
-bool Image::FillFromPgm(const std::string& file_name)
-{
-  igg::io_tools::ImageData image = igg::io_tools::ReadFromPgm(file_name);
-  data_ = image.data;
-  rows_ = image.rows;
-  cols_ = image.cols;
-  if (!data_.empty())
-  {
-    return true;
-  }
-  else
-  {
-    std::cerr << "[ERROR] Could not load data!\n";
-    return false;
-  }
-}
-
-void Image::WriteToPgm(const std::string& file_name)
-{
-  igg::io_tools::ImageData image;
-  image.data = data_;
-  image.rows = rows_;
-  image.cols = cols_;
-  bool written = igg::io_tools::WriteToPgm(image, file_name);
-}
-
-std::vector<float> Image::ComputeHistogram(int bins) const
-{
-  int binSize = 255 / bins;
-  std::vector<float> histogram(bins, 0);
-
-  // for each element of data_
-  for (int i = 0; i < data_.size(); i++)
-  {
-    // for each bin
-    for (int z = 0; z < bins; z++)
-    {
-      // check both upper and lower limits
-      if (data_[i] >= z * binSize && data_[i] < (z + 1) * binSize)
-      {
-        // increment the index that contains the point
-        histogram[z]++;
-      }
-    }
-  }
-  return histogram;
-}
-
-void Image::DownScale(int scale)
-{
-  std::vector<uint8_t> scaled_data(((rows_ + 1) / scale) * ((cols_ + 1) / scale), 0);
-  for (int r = 0; r < (rows_ / scale); r++)
-  {
-    for (int c = 0; c < (cols_ / scale); c++)
-    {
-      scaled_data[r * (cols_ / scale) + c] = at(r * scale, c * scale);
-    }
-  }
-  data_ = scaled_data;
-  rows_ = rows_ / scale;
-  cols_ = cols_ / scale;
-}
-
-void Image::UpScale(int scale)
-{
-  std::vector<uint8_t> scaled_data(((rows_ + 1) * scale) * ((cols_ + 1) * scale), 0);
-  for (int r = 0; r < (rows_ * scale); r++)
-  {
-    for (int c = 0; c < (cols_ * scale); c++)
-    {
-      scaled_data[r * (cols_ * scale) + c] = at(std::floor(r / scale), std::floor(c / scale));
-    }
-  }
-  data_ = scaled_data;
-  rows_ = rows_ * scale;
-  cols_ = cols_ * scale;
 }
